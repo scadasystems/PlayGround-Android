4acff5aaf7ad86fc65d84e0ea951c4e62944629a
LulzM <yunmo159789@gmail.com>
2020-03-02 16:02:11 +0900

    Play Json Products

---
+++ Play-json-TAG/app/src/main/java/com/iloveintouch/demo2/Products.kt
@@ -0,0 +1,19 @@
+package com.iloveintouch.demo2
+
+/*********************************************************
+ *   ,--.           ,--.       ,--.   ,--.
+ *   |  |   ,--.,--.|  |,-----.|   `.'   |
+ *   |  |   |  ||  ||  |`-.  / |  |'.'|  |
+ *   |  '--.'  ''  '|  | /  `-.|  |   |  |
+ *   `-----' `----' `--'`-----'`--'   `--'
+ *
+ * Project : PlayJsonTag
+ * Created by Android Studio
+ * Developer : Lulz_M
+ * Date : 2020/03/02
+ * Time : 3:43 PM
+ * GitHub : https://github.com/scadasystems
+ * E-mail : redsmurf@lulzm.org
+ *********************************************************/
+ 
+ 
