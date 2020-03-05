d64df6396b475c3dcaa39725ab049b60d2a8b230
LulzM <yunmo159789@gmail.com>
2020-03-05 11:18:18 +0900

    Add example json and Touch event

---
+++ Play-json-TAG/app/src/main/java/com/iloveintouch/demo2/exam/TouchActivity.kt
@@ -0,0 +1,13 @@
+package com.iloveintouch.demo2.exam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iloveintouch.demo2.R
+
+class TouchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_touch)
+    }
+}
